9217aaef2155558f6e8edb1d2db4b367945a1564
lhw <2458858175@qq.com>
2020-07-28 23:38:19 +0800

    Actor_Critic: updated AC_continue_pendulm

--- Actor_Critic/1_Actor_Critic/AC_continue_Pendulum.py
+++ Actor_Critic/1_Actor_Critic/AC_continue_Pendulum.py
@@ -9,12 +9,13 @@ np.random.seed(2)
 torch.manual_seed(2) 
 
 class ActorNet(nn.Module):
-  def __init__(self, n_states, n_actions, n_middle=30):
+  def __init__(self, action_bound, n_states, n_actions, n_middle=30):
     super(ActorNet, self).__init__()
+    self.action_bound = action_bound
     self.l1 = nn.Linear(n_states, n_middle)
     self.l1.weight.data.normal_(0, 0.1)
     self.mu = nn.Linear(n_middle, 1)
-    self.mu.weight.data.mormal_(0. 0.1)
+    self.mu.weight.data.normal_(0, 0.1)
     self.sigma = nn.Linear(n_middle, 1)
     self.sigma.weight.data.normal_(0, 0.1)
 
@@ -22,26 +23,39 @@ class ActorNet(nn.Module):
     x = self.l1(x)
     x = F.relu(x)
     x_mu = self.mu(x)
-    x_mu = F.tanh(x_mu)
+    x_mu = torch.tanh(x_mu)
     x_sigma = self.sigma(x)
     '''
      a = torch.Tensor([[[[1], [2], [4]]]])  torch.Size([1, 1, 3, 1])
      b = torch.squeeze(a)                   torch.Size([3])
     '''
     x_mu, x_sigma = torch.squeeze(x_mu*2), torch.squeeze(x_sigma+0.1)
+    normal_list = torch.distributions.Normal(x_mu, x_sigma)
+    action = torch.clamp(normal_list.sample((1,)), self.action_bound[0][0], self.action_bound[1][0])
+    return action, normal_list
 
-    
 
-    self.fc2 = nn.Linear(n_middle, n_actions)
-    self.fc2.weight.data.normal_(0, 0.1)
+class Actor(object):
+  def __init__(self, n_features, action_bound, lr=0.001):
+    self.n_features = n_features
+    self.net = ActorNet(action_bound, n_features, 1)
+    self.optimizer = torch.optim.Adam(self.net.parameters(), lr=lr)
+
+  def learn(self, s, a, td):
+    action, normal_list = self.net(torch.Tensor(s[np.newaxis, :]))
+    log_prob = normal_list.log_prob(action)
+    exp_v = log_prob * torch.Tensor(td)
+    exp_v += 0.01 * normal_list.entropy()
+    loss = torch.min(-exp_v, torch.Tensor([0]))
   
-  def forward(self, x):
-    x = self.fc1(x)
-    x = F.relu(x, inplace=True)
-    x = self.fc2(x)
-    output = F.tanh(x, dim=1)
-    # output = F.softmax(x, dim=1)
-    return output
+    self.optimizer.zero_grad()
+    loss.backward()
+    self.optimizer.step()
+    return exp_v
+
+  def choose_action(self, observation):
+    action, _ = self.net(torch.Tensor(observation[np.newaxis, :]))
+    return action
 
 class CriticNet(nn.Module):
   def __init__(self, n_states, n_middle=30):
@@ -56,29 +70,6 @@ class CriticNet(nn.Module):
     x = F.relu(x, inplace=True)
     output = self.fc2(x)
     return output
-
-class Actor(object):
-  def __init__(self, n_features, n_actions, lr=0.001):
-    self.n_features = n_features
-    self.n_actions = n_actions
-    self.net = ActorNet(n_features, n_actions)
-    self.optimizer = torch.optim.Adam(self.net.parameters(), lr=lr)
-
-  def learn(self, s, a, td):
-    output = self.net(torch.Tensor(s[np.newaxis, :]))
-    one_hot = F.one_hot(torch.arange(0, self.n_actions))[a]
-    # one_hot = torch.zeros(1, self.n_actions).scatter_(1, torch.LongTensor([a]).view(-1, 1), 1)
-    neg = torch.sum(-torch.log(output) * one_hot, 1)
-    loss = (neg * torch.Tensor(td)).mean()
-
-    self.optimizer.zero_grad()
-    loss.backward()
-    self.optimizer.step()
-
-  def choose_action(self, observation):
-    prob_weights = self.net(torch.Tensor(observation[np.newaxis, :]))
-    action = np.random.choice(range(prob_weights.shape[1]), p=prob_weights.view(-1).detach().numpy())
-    return action
 
 class Critic(object):
   def __init__(self, n_features, lr=0.01):
@@ -100,9 +91,8 @@ class Critic(object):
 
     return td_error.data.numpy()
   
-# Superparameters
-MAX_EPISODE = 3000
-DISPLAY_REWARD_THRESHOLD = 200  # renders environment if total episode reward is greater then this threshold
+MAX_EPISODE = 1000
+DISPLAY_REWARD_THRESHOLD = -100  # renders environment if total episode reward is greater then this threshold
 MAX_EP_STEPS = 1000   # maximum time step in one episode
 RENDER = False  # rendering wastes time
 GAMMA = 0.9     # reward discount in TD error
@@ -114,23 +104,21 @@ env.seed(1)  # reproducible
 env = env.unwrapped
 
 N_F = env.observation_space.shape[0]
-N_A = env.action_space.n
+A_BOUND = env.action_space.high
 
-actor = Actor(n_features=N_F, n_actions=N_A, lr=LR_A)
+actor = Actor(n_features=N_F, lr=LR_A, action_bound=[-A_BOUND, A_BOUND])
 critic = Critic(n_features=N_F, lr=LR_C)
 
 for i_episode in range(MAX_EPISODE):
   s = env.reset()
   t = 0
-  track_r = []
+  ep_rs = []
   while True:
     if RENDER: env.render()
 
     a = actor.choose_action(s)
     s_, r, done, info = env.step(a)
-
-    if done: r = -20
-    track_r.append(r)
+    r /= 10
 
     td_error = critic.learn(s, r, s_)  # gradient = grad[r + gamma * V(s_) - V(s)]
     actor.learn(s, a, td_error)     # true_gradient = grad[logPi(s,a) * td_error]
@@ -138,12 +126,14 @@ for i_episode in range(MAX_EPISODE):
     s = s_
     t += 1
 
+    ep_rs.append(r)
+
     if done or t >= MAX_EP_STEPS:
-      ep_rs_sum = sum(track_r)
+      ep_rs_sum = sum(ep_rs)
       if 'running_reward' not in globals():
         running_reward = ep_rs_sum
       else:
-        running_reward = running_reward * 0.95 + ep_rs_sum * 0.05
+        running_reward = running_reward * 0.9 + ep_rs_sum * 0.1
       if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True  # rendering
       print("episode:", i_episode, "  reward:", int(running_reward))
       break
